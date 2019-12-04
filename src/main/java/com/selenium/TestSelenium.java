6bb3a935383ba2b02bf6a452dbcdc92a3f41820e
Aayush Attri <aayush.attri@ofbusiness.in>
2019-12-04 15:29:23 +0530

    detailHtml and docs stored in folders

--- src/main/java/com/selenium/TestSelenium.java
+++ src/main/java/com/selenium/TestSelenium.java
@@ -1,14 +1,23 @@
 package com.selenium;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.pagefactory.ByChained;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -19,19 +28,40 @@ public class TestSelenium {
 
     public static void hitUrl(){
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://etender.ongc.co.in/irj/portal");
+        WebDriver driver1 = new ChromeDriver();
+        driver1.get("https://etender.ongc.co.in/irj/portal");
 
-        driver.findElement(By.cssSelector("input[name=Guest]")).click();
+        driver1.findElement(By.cssSelector("input[name=Guest]")).click();
 
         try{ Thread.sleep(10000); }
         catch(InterruptedException ex){Thread.currentThread().interrupt();}
 
-        List<String> idList = parseList(driver);
-        String listPage = driver.getWindowHandle();// save list page
-        List<String> detailPagesHtml = new LinkedList<>();
+        List<String> idList = parseList(driver1);
+        driver1.close();
+
+        File file1 = new File("/home/aayush/Desktop/Ongc/");
+        file1.mkdir();
+
 
         for (int i=0;i<idList.size();i++){
+
+            String path = String.format("/home/aayush/Desktop/Ongc/%s/",idList.get(i));
+            File file = new File(path);
+            file.mkdir();
+
+            ChromeOptions options = new ChromeOptions();
+            Map<String,Object> prefs = new HashMap<>();
+            prefs.put("profile.default_content_settings.popups", 0);
+            prefs.put("download.default_directory",path);
+            options.setExperimentalOption("prefs",prefs);
+            WebDriver driver = new ChromeDriver(options);
+            driver.get("https://etender.ongc.co.in/irj/portal");
+            driver.findElement(By.cssSelector("input[name=Guest]")).click();
+            try{ Thread.sleep(10000); }
+            catch(InterruptedException ex){Thread.currentThread().interrupt();}
+            String listPage = driver.getWindowHandle();// save list page
+
+            driver.switchTo().defaultContent().switchTo().frame("contentAreaFrame").switchTo().frame("Bid Processing");
 
             String xpath1 = String.format("//*[contains(text(),'%s')]",idList.get(i));
             driver.findElement(By.xpath(xpath1)).click();
@@ -44,11 +74,18 @@ public class TestSelenium {
                 driver.switchTo().window(winHandle);
             }
 
-             detailPagesHtml.add(driver.getPageSource());
-
             driver.switchTo().defaultContent().switchTo().frame("contentAreaFrame").switchTo().frame("isolatedWorkArea");
 
             String detailPage = driver.getWindowHandle(); // save detail page
+
+            String detailHtml = driver.getPageSource();
+            try {
+                File file2 = new File(path+"detailPageHtml.txt");
+                FileWriter fileWriter = new FileWriter(file2);
+                fileWriter.write(detailHtml);
+                fileWriter.flush();
+                fileWriter.close();
+            }catch (Exception e){}
 
             driver.findElement(By.id("WD1D-r")).click();
             try{ Thread.sleep(10000); }
@@ -153,15 +190,13 @@ public class TestSelenium {
 
             }
 
-            //close all windows except listPage
+            //close all windows
             driver.switchTo().window(docListPage);
             driver.close();
             driver.switchTo().window(detailPage);
             driver.close();
-
-
             driver.switchTo().window(listPage);
-            driver.switchTo().defaultContent().switchTo().frame("contentAreaFrame").switchTo().frame("Bid Processing");
+            driver.close();
 
 
 
